2784c0e4d27ef8ab4c5c66541f791524b1a938c8
Computhink-CS143 <cs143@computhink.com.sg>
2025-12-16 06:07:26 +0000

    day 1

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,7 +1,10 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day1")
-
+print("hello from day1! I am Bryan.")
+print("I love swimming.")
+print("I live in canberra")
+print("my school is CHS")
+print("if i have $2000,i would buy gold,shares and bonds")
 ########################################################################
 # Task 1:
 
@@ -9,16 +12,31 @@ print("hello from day1")
 
 ########################################################################
 # Task 2:
-
+var1=10
+var2=2
+print(var1+var2,"=","10","+","2")
+print(var1*var2,"=","10","x","2")
+print(var1-var2,"=","10","-","2")
+print(var1/var2,"=","10","/","2")
 
 
 ########################################################################
 # Task 3:
-
+name=" BRYAN"
+print("My name is"+name)
+food=" potatoes"
+print("I like to eat"+food)
 
 
 ########################################################################
 # Task 4:
+def greetings(variable):
+    print("Hello,I am Bryan.")
+    print("I love to swim.")
+    print("Aditionally,my school is CHS.")
+    print("Nice to meet you,"+ variable)
+
+greetings("GER")
 
 
 
--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -1,6 +1,66 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
 ################# Function Section #############################
+def addition(num1,num2):
+    player.say(num1+num2)
 
+
+def multiply(num,num8):
+    player.say(num*num8)
+
+
+
+
+addition(23,34)
+
+
+
+
+
+def teleport():
+    agent.teleport_to_player()
+player.on_chat("come", teleport)
+
+
+
+#tr:turn right
+def tr():
+    agent.turn(TurnDirection.RIGHT)
+player.on_chat("tr", tr)
+
+#tl:turn left
+def tl():
+    agent.turn(TurnDirection.LEFT)
+player.on_chat("tl",tl)def addition(num1,num2):
+    player.say(num1+num2)
+
+
+def multiply(num,num8):
+    player.say(num*num8)
+
+
+
+
+addition(23,34)
+
+
+
+
+
+def teleport():
+    agent.teleport_to_player()
+player.on_chat("come", teleport)
+
+
+
+#tr:turn right
+def tr():
+    agent.turn(TurnDirection.RIGHT)
+player.on_chat("tr", tr)
+
+#tl:turn left
+def tl():
+    agent.turn(TurnDirection.LEFT)
+player.on_chat("tl",tl)
 
 ################## On Chat Commands Section #####################
