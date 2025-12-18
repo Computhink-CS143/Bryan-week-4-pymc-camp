9e83ba23796fc27fa89ab4c3d8034bb937ba3b46
Computhink-CS143 <cs143@computhink.com.sg>
2025-12-18 07:14:01 +0000

    Dec 18, 2025, 7:14 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -1,9 +1,14 @@
 # Write all your codes for Day 3 here.
 # COMMENT out the previous task before going on to the next task
 print("hello from day3")
-
 ########################################################################
 # Task 1:
+
+# yourname = input("what is your name?")
+# # title = input("what is your title?")
+# task = input("what is the task?")
+
+# print(title, yourname, "orders the peasants to", task)
 
 
 
@@ -14,6 +19,10 @@ print("hello from day3")
 
 ########################################################################
 # Task 3:
+# num1=int(input('first no.:'))
+# num2=int(input('second No.:'))
+# print(num1,"+",num2,'=',num1+num2)
+# print(num1,"*",num2,'=',num1*num2)
 
 
 
@@ -25,10 +34,27 @@ print("hello from day3")
 ########################################################################
 # Task 5:
 
+# password='not'
+# guess=input('input password:')
+
+# if guess==password:
+#     print('please come in')
+
+# else:
+#     print('I am contacting INTERPOL')
+
 
 
 ########################################################################
 # Task 6:
+legal=20
+age=int(input('your age:'))
+
+if age>legal:
+    print('can vote')
+
+else:
+    print('cannot vote')
 
 
 
@@ -44,3 +70,32 @@ print("hello from day3")
 
 ########################################################################
 # Additional exercises:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Day3Folder/minecraft_day3.py
+++ Day3Folder/minecraft_day3.py
@@ -1,2 +1,107 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+
+
+ 
+  
+   
+    
+     def teleport():
+    agent.teleport_to_player()
+player.on_chat("come", teleport)
+
+
+
+#tr:turn right
+def tr():
+    agent.turn(TurnDirection.RIGHT)
+player.on_chat("tr", tr)
+
+#tl:turn left
+def tl():
+    agent.turn(TurnDirection.LEFT)
+player.on_chat("tl",tl)
+
+
+
+#move forward
+def foward(count):
+    agent.move(FORWARD, count)
+player.on_chat("fw", foward)
+#move up
+def o(count):
+    agent.move(UP, count)
+player.on_chat("up", o)
+#move down
+def down(count):
+    agent.move(DOWN, count)
+player.on_chat("down", down)
+#playground forwards
+def obby1():
+    agent.move(FORWARD, 4)
+    agent.turn(TurnDirection.LEFT)
+    agent.move(FORWARD, 4)
+    agent.turn(TurnDirection.RIGHT)
+    agent.move(FORWARD, 3)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 3)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+player.on_chat('obby1', obby1)
+
+#playground backwards
+def obby2():
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 3)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 4)
+    agent.turn(TurnDirection.LEFT)
+    agent.move(FORWARD, 4)
+    agent.turn(TurnDirection.RIGHT)
+    agent.move(FORWARD, 4)
+player.on_chat("ob2", obby2)
+
+
+
+
+def chop(ht):
+    agent.destroy(FORWARD)
+    agent.move(FORWARD, 1)
+    for count in range(ht):
+        agent.destroy(UP)
+        agent.collect_all()
+        agent.move(UP, 1)
+    agent.teleport_to_player()
+player.on_chat("chop", chop)
+
+
+
+
+
+def mine(count1):
+    for count in range(count1):
+        agent.destroy(FORWARD)
+        agent.collect_all()
+        agent.destroy(LEFT)
+        agent.collect_all()
+        agent.destroy(RIGHT)
+        agent.collect_all()
+        agent.destroy(DOWN)
+        agent.collect_all()
+        agent.move(FORWARD, 1)
+player.on_chat("destory", mine)
